ddbd7a51472972003980c01548b2de55c4c3fdbd
Jin Choi <seoyeonjin.choi.ext@allianz-trade.com>
2025-06-25 15:25:23 +0200

    test

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 export default function UserList() {
+  const { data, errors, loading } = useFetch();
   //use fetch and @tanstack/react-query to make an GET request to https://jsonplaceholder.typicode.com/users and handle 3 cases, error, loading and success
 
-  return <>map userData </>;
+  if (!errors) return <>Error</>;
+  if (loading) return <>isLoading</>;
+  return <>{data} </>;
 }
+
+export const useFetch = async () => {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState<any>(null);
+
+  try {
+    await fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      setData(res.json())
+    );
+    setLoading(false);
+  } catch (error) {
+    setErrors(error);
+  }
+
+  return { data, loading, errors };
+};
