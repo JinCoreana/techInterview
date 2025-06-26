183f0d7e8206514e7d13be76ae65a90f835af899
Jin Choi <seoyeonjin.choi.ext@allianz-trade.com>
2025-06-26 12:08:41 +0200

    interview

--- src/components/UserList.test.tsx
+++ src/components/UserList.test.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import "@testing-library/jest-dom";
+import UserList from "./UserList";
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Use "@testing-library/react" and test 3 cases
 // Do not mock useQuery but only mock fetcj
-global.fetch = jest.fn();
+global.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve(() => {
+    setTimeout(() => {}, 20000);
+  })
+);
 
 const mockData = [
   { id: 1, name: "John Doe", email: "john@example.com" },
   { id: 2, name: "Jane Smith", email: "jane@example.com" },
 ];
 
+const queryClient = new QueryClient();
+const renderComponent = () => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <UserList />
+    </QueryClientProvider>
+  );
+};
 describe("UserList", () => {
-  test("renders loading state initially", () => {});
+  test("renders loading state initially", () => {
+    renderComponent();
+    expect();
+  });
 
   test("renders users after successful fetch", () => {});
 
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+
 export default function UserList() {
   //use fetch and @tanstack/react-query to make an GET request to https://jsonplaceholder.typicode.com/users and handle 3 cases, error, loading and success
-
-  return <>map userData </>;
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["mock"],
+    queryFn: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(data);
+  if (isError) return <>error</>;
+  return (
+    <>
+      {data?.map((obj: any) => {
+        return <p>{obj.name}</p>;
+      })}
+    </>
+  );
 }
